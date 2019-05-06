494b86fdcbb437a412623f787e6c4e632d7d6062
yaelhava <yaelhava2610@gmail.com>
2019-05-06 10:50:43 +0300

    update

--- SmartGuesser.cpp
+++ SmartGuesser.cpp
@@ -24,75 +24,79 @@ void SmartGuesser::learn(string s)
 
 void SmartGuesser::startNewGame(uint length)
 {
+    this->length = length;
 
     if (!list.empty())
     {
         list.clear();
     }
 
-    switch (length)
+    if (length < 5)
     {
-
-    case 1:
-        for (int i = 0; i < 10; i++)
+        switch (length)
         {
-            list.push_back(to_string(i));
-        }
 
-        break;
-
-    case 2:
-        for (int i = 0; i < 100; i++)
-        {
-            if (i < 10)
-            {
-                list.push_back('0' + to_string(i));
-            }
-            else
+        case 1:
+            for (int i = 0; i < 10; i++)
             {
                 list.push_back(to_string(i));
             }
+
+            break;
+
+        case 2:
+            for (int i = 0; i < 100; i++)
+            {
+                if (i < 10)
+                {
+                    list.push_back('0' + to_string(i));
+                }
+                else
+                {
+                    list.push_back(to_string(i));
+                }
+            }
+            break;
+        case 3:
+            for (int i = 0; i < 1000; i++)
+            {
+                if (i < 10)
+                {
+                    list.push_back("00" + to_string(i));
+                }
+                else if (i < 100)
+                {
+                    list.push_back('0' + to_string(i));
+                }
+                else
+                {
+                    list.push_back(to_string(i));
+                }
+            }
+            break;
+        case 4:
+            for (int i = 0; i < 10000; i++)
+            {
+                if (i < 10)
+                {
+                    list.push_back("000" + to_string(i));
+                }
+                else if (i < 100)
+                {
+                    list.push_back("00" + to_string(i));
+                }
+                else if (i < 1000)
+                {
+                    list.push_back('0' + to_string(i));
+                }
+                else
+                {
+                    list.push_back(to_string(i));
+                }
+            }
+            break;
+        
         }
-        break;
-    case 3:
-        for (int i = 0; i < 1000; i++)
-        {
-            if (i < 10)
-            {
-                list.push_back("00" + to_string(i));
-            }
-            else if (i < 100)
-            {
-                list.push_back('0' + to_string(i));
-            }
-            else
-            {
-                list.push_back(to_string(i));
-            }
-        }
-        break;
-    case 4:
-        for (int i = 0; i < 10000; i++)
-        {
-            if (i < 10)
-            {
-                list.push_back("000" + to_string(i));
-            }
-            else if (i < 100)
-            {
-                list.push_back("00" + to_string(i));
-            }
-            else if (i < 1000)
-            {
-                list.push_back('0' + to_string(i));
-            }
-            else
-            {
-                list.push_back(to_string(i));
-            }
-        }
-        break;
-    default:
-        break;
     }
+    else{} //for length greater then 4
 }
