c9b4da76c4cd4c7729e5c66fee8d9fe6f05e3116
yaelhava <yaelhava2610@gmail.com>
2019-05-06 11:17:45 +0300

    update

--- SmartGuesser.cpp
+++ SmartGuesser.cpp
@@ -31,72 +31,67 @@ void SmartGuesser::startNewGame(uint length)
         list.clear();
     }
 
-    if (length < 5)
+    switch (length)
     {
-        switch (length)
-        {
 
-        case 1:
-            for (int i = 0; i < 10; i++)
+    case 1:
+        for (int i = 0; i < 10; i++)
+        {
+            list.push_back(to_string(i));
+        }
+
+        break;
+
+    case 2:
+        for (int i = 0; i < 100; i++)
+        {
+            if (i < 10)
+            {
+                list.push_back('0' + to_string(i));
+            }
+            else
             {
                 list.push_back(to_string(i));
             }
-
-            break;
-
-        case 2:
-            for (int i = 0; i < 100; i++)
-            {
-                if (i < 10)
-                {
-                    list.push_back('0' + to_string(i));
-                }
-                else
-                {
-                    list.push_back(to_string(i));
-                }
-            }
-            break;
-        case 3:
-            for (int i = 0; i < 1000; i++)
-            {
-                if (i < 10)
-                {
-                    list.push_back("00" + to_string(i));
-                }
-                else if (i < 100)
-                {
-                    list.push_back('0' + to_string(i));
-                }
-                else
-                {
-                    list.push_back(to_string(i));
-                }
-            }
-            break;
-        case 4:
-            for (int i = 0; i < 10000; i++)
-            {
-                if (i < 10)
-                {
-                    list.push_back("000" + to_string(i));
-                }
-                else if (i < 100)
-                {
-                    list.push_back("00" + to_string(i));
-                }
-                else if (i < 1000)
-                {
-                    list.push_back('0' + to_string(i));
-                }
-                else
-                {
-                    list.push_back(to_string(i));
-                }
-            }
-            break;
-        
         }
+        break;
+    case 3:
+        for (int i = 0; i < 1000; i++)
+        {
+            if (i < 10)
+            {
+                list.push_back("00" + to_string(i));
+            }
+            else if (i < 100)
+            {
+                list.push_back('0' + to_string(i));
+            }
+            else
+            {
+                list.push_back(to_string(i));
+            }
+        }
+        break;
+    case 4:
+        for (int i = 0; i < 10000; i++)
+        {
+            if (i < 10)
+            {
+                list.push_back("000" + to_string(i));
+            }
+            else if (i < 100)
+            {
+                list.push_back("00" + to_string(i));
+            }
+            else if (i < 1000)
+            {
+                list.push_back('0' + to_string(i));
+            }
+            else
+            {
+                list.push_back(to_string(i));
+            }
+        }
+        break;
     }
-    else{} //for length greater then 4
 }
--- Test.cpp
+++ Test.cpp
@@ -11,6 +11,7 @@ using namespace std;
 
 #include "play.hpp"
 #include "DummyChoosers.hpp"
+
 #include "DummyGuessers.hpp"
 #include "SmartGuesser.hpp"
 #include "badkan.hpp"
@@ -26,80 +27,84 @@ int main() {
 	if (signal == 0) {
 
 		// BASIC TESTS - DO NOT CHANGE
-		ConstantChooser c1234{"1234"}, c12345{"12345"}, c9999{"9999"}, c001("001"), c987654321{"987654321"}, c9876{"9876"};
-		ConstantGuesser g1234{"1234"}, g12345{"12345"}, g9999{"9999"}, g1{"1"}, g123456789{"123456789"},g9876{"9876"},
-		inputEr{"jklj777"}, inputEr1{"-999"};
+		ConstantChooser c1234{"1234"}, c12345{"12345"}, c9999{"9999"}, c2610{"2610"}, c0406{"0406"}, c98765{"98765"};
+		ConstantGuesser g1234{"1234"}, g12345{"12345"}, g9999{"9999"} , g2610{"2610"}, g0406{"0406"}, g98765{"98765"};
 
 		testcase.setname("Calculate bull and pgia")
 		.CHECK_OUTPUT(calculateBullAndPgia("1234","1234"), "4,0")      // 4 bull, 0 pgia
 		.CHECK_OUTPUT(calculateBullAndPgia("1234","4321"), "0,4")      // 0 bull, 4 pgia
-		.CHECK_OUTPUT(calculateBullAndPgia("0000","9999"), "0,0")
-		.CHECK_OUTPUT(calculateBullAndPgia("111","111"), "3,0")
-		.CHECK_OUTPUT(calculateBullAndPgia("555","655"), "2,0")
-		//.CHECK_OUTPUT(calculateBullAndPgia("001","1"), "0,1") 
-		//.CHECK_OUTPUT(calculateBullAndPgia("010","1"), "0,1")     
-       //         .CHECK_OUTPUT(calculateBullAndPgia("100","1"), "1,0")
-		.CHECK_OUTPUT(calculateBullAndPgia("4","1"), "0,0") 
-                .CHECK_OUTPUT(calculateBullAndPgia("1","1"), "1,0")
-                .CHECK_OUTPUT(calculateBullAndPgia("01","10"), "0,2")
-  		.CHECK_OUTPUT(calculateBullAndPgia("10","10"), "2,0")
-                .CHECK_OUTPUT(calculateBullAndPgia("9876543210","9876543210"), "10,0")  
-                .CHECK_OUTPUT(calculateBullAndPgia("987654321","123456789"), "1,8") 
-	        .CHECK_OUTPUT(calculateBullAndPgia("666","11176"), "0,0") 
-		.CHECK_OUTPUT(calculateBullAndPgia("4567","7654"), "0,4") 
-		.CHECK_OUTPUT(calculateBullAndPgia("4","7654"), "0,0") 
-		.CHECK_OUTPUT(calculateBullAndPgia("7","7654"), "1,0") 
-		.CHECK_OUTPUT(calculateBullAndPgia("7","60"), "0,0") 
-		//.CHECK_OUTPUT(calculateBullAndPgia("7777","77"), "2,0") 
-
-
-
-
-
-	 	;
+		.CHECK_OUTPUT(calculateBullAndPgia("2610","2610"), "4,0")      // 4 bull, 0 pgia
+		.CHECK_OUTPUT(calculateBullAndPgia("2610","1234"), "0,2")      // 0 bull, 2 pgia
+		.CHECK_OUTPUT(calculateBullAndPgia("0406","0406"), "4,0")      // 4 bull, 0 pgia
+		.CHECK_OUTPUT(calculateBullAndPgia("0406","3657"), "0,1")      // 0 bull, 1 pgia
+		.CHECK_OUTPUT(calculateBullAndPgia("1111","1112"), "3,0")      // 3 bull, 0 pgia
+		.CHECK_OUTPUT(calculateBullAndPgia("1212","1234"), "2,0")      // 2 bull, 0 pgia
+		.CHECK_OUTPUT(calculateBullAndPgia("8777","8778"), "3,0")      // 3 bull, 0 pgia
+		.CHECK_OUTPUT(calculateBullAndPgia("8777","7778"), "2,2")      // 2 bull, 2 pgia
+		.CHECK_OUTPUT(calculateBullAndPgia("12345","12345"), "5,0")      	// 5 bull, 0 pgia
+		.CHECK_OUTPUT(calculateBullAndPgia("12234","12324"), "3,2")      	// 3 bull, 2 pgia
+		.CHECK_OUTPUT(calculateBullAndPgia("4444","1234"), "1,0")      		// 1 bull, 0 pgia
+		.CHECK_OUTPUT(calculateBullAndPgia("0000","1234"), "0,0")      		// 0 bull, 0 pgia
+		.CHECK_OUTPUT(calculateBullAndPgia("433434","123456"), "2,0")     // 2 bull, 0 pgia
+		.CHECK_OUTPUT(calculateBullAndPgia("433434","434343"), "2,4")     // 2 bull, 4 pgia
+		.CHECK_OUTPUT(calculateBullAndPgia("12345678","87655678"), "4,0") // 4 bull, 0 pgia
+		.CHECK_OUTPUT(calculateBullAndPgia("2610","0406"), "0,2")      		// 0 bull, 2 pgia
+		;
 
 		testcase.setname("Play with dummy choosers and guessers")
 		.CHECK_EQUAL(play(c1234, g1234, 4, 100), 1)      // guesser wins in one turn.
 		.CHECK_EQUAL(play(c1234, g9999, 4, 100), 101)    // guesser loses by running out of turns 
 		.CHECK_EQUAL(play(c1234, g12345, 4, 100), 101)   // guesser loses technically by making an illegal guess (too long).
 		.CHECK_EQUAL(play(c12345, g1234, 4, 100), 0)     // chooser loses technically by choosing an illegal number (too long).
-		.CHECK_EQUAL(play(c001, g1, 3, 100), 101)     // guesser loses technically by choosing an illegal number (too long).
-		.CHECK_EQUAL(play(c9999, g123456789, 4, 100),101 )     // guesser loses technically by choosing an illegal number (too long).
-		.CHECK_EQUAL(play(c1234, g123456789, 9, 100), 0)     // chooser loses technically by choosing an illegal number (too long).
-		.CHECK_EQUAL(play(c9876, g9876, 4, 100), 1)//gusser wins in one turn
-		.CHECK_EQUAL(play(c1234, g9999, 4, 57), 58)    // guesser loses by running out of turns
-                .CHECK_EQUAL(play(c987654321, g123456789, 9, 12), 13)    // guesser loses by running out of turns 
+		
+		.CHECK_EQUAL(play(c2610, g2610, 4, 100), 1)      // guesser wins in one turn.
+		.CHECK_EQUAL(play(c2610, g0406, 4, 100), 101)    // guesser loses by running out of turns 
+		.CHECK_EQUAL(play(c0406, g2610, 4, 100), 101)    // guesser loses by running out of turns
+		.CHECK_EQUAL(play(c0406, g0406, 4, 100), 1)      // guesser wins in one turn.
+		.CHECK_EQUAL(play(c98765, g0406, 4, 100), 0)     // chooser loses technically by choosing an illegal number (too long).
+		.CHECK_EQUAL(play(c2610, g98765, 4, 100), 101)   // guesser loses technically by making an illegal guess (too long).   
+		.CHECK_EQUAL(play(c9999, g9999, 4, 100), 1)      // guesser wins in one turn.
+		.CHECK_EQUAL(play(c9999, g0406, 4, 100), 101)    // guesser loses by running out of turns 
+		.CHECK_EQUAL(play(c9999, g2610, 4, 100), 101)    // guesser loses by running out of turns
+		.CHECK_EQUAL(play(c0406, g98765, 4, 100), 101)   // guesser loses technically by making an illegal guess (too long). 
+		.CHECK_EQUAL(play(c98765, g2610, 4, 100), 0)     // chooser loses technically by choosing an illegal number (too long).
 
+		.CHECK_EQUAL(play(c9999, g9999, 4, 100), 1)      // guesser wins in one turn.
+		.CHECK_EQUAL(play(c1234, g2610, 4, 100), 101)    // guesser loses by running out of turns
+		.CHECK_EQUAL(play(c0406, g9999, 4, 100), 101)    // guesser loses by running out of turns
+		.CHECK_EQUAL(play(c0406, g1234, 4, 100), 101)    // guesser loses by running out of turns
+		.CHECK_EQUAL(play(c9999, g1234, 4, 100), 101)    // guesser loses by running out of turns
+		.CHECK_EQUAL(play(c12345, g98765, 5, 100), 101)  // guesser loses by running out of turns
 
-
+		.CHECK_EQUAL(play(c9999, g9999, 5, 100), 0)      // chooser loses technically by choosing an illegal number (too short).
+		.CHECK_EQUAL(play(c9999, g12345, 5, 100), 0)     // chooser loses technically by choosing an illegal number (too short).
+		.CHECK_EQUAL(play(c12345, g9999, 5, 100), 101)     // guesser loses technically by making an illegal guess (too short).
+		.CHECK_EQUAL(play(c12345, g98765, 4, 100), 0)    // chooser loses technically by choosing an illegal number (too long).
+		.CHECK_EQUAL(play(c1234, g2610, 4, 90), 91)    	 // guesser loses by running out of turns
+		.CHECK_EQUAL(play(c0406, g9999, 4, 10), 11)    	 // guesser loses by running out of turns
+		.CHECK_EQUAL(play(c0406, g1234, 4, 6), 7)    		 // guesser loses by running out of turns
+		.CHECK_EQUAL(play(c9999, g1234, 4, 300), 301)    // guesser loses by running out of turns
+		.CHECK_EQUAL(play(c12345, g98765, 5, 56), 57)  	 // guesser loses by running out of turns
+		.CHECK_EQUAL(play(c9999, g9999, 4, 2), 1)      	 // guesser wins in one turn.
 		;
 
-		testcase.setname("Play with smart guesser");
 
+		testcase.setname("Play with smart guesser");
 		RandomChooser randy;
 		SmartGuesser smarty;
 		for (uint i=0; i<100; ++i) {
-			testcase.CHECK_EQUAL(play(randy, smarty, 4, 100)<=100, true);  // smarty should always win in at most 100 turns!
-		}
-		
-		RandomChooser randy1;
-		SmartGuesser smarty1;
-		for (uint i=0; i<100; ++i) {
-			testcase.CHECK_EQUAL(play(randy1, smarty1, 4, 100)>100, false);  // smarty should always win in at most 100 turns!
+			testcase.CHECK_EQUAL(play(randy, smarty, 4, 100) <= 100, true);  // smarty should always win in at most 100 turns!
 		}
 
-		RandomChooser randy5;
-		SmartGuesser smarty5;
-		for (uint i=0; i<100; ++i) {
-			testcase.CHECK_EQUAL(play(randy5, smarty5, 4, 100)<=100, true);  // smarty should always win in at most 100 turns!
-		}
-		
-		RandomChooser randy9;
-		SmartGuesser smarty9;
-		for (uint i=0; i<100; ++i) {
-			testcase.CHECK_EQUAL(play(randy9, smarty9, 4, 100)<=100, true);  // smarty should always win in at most 100 turns!
-		}
-		
+		testcase
+		.CHECK_EQUAL(play(c1234, smarty, 4, 100) <= 100, true)  
+		.CHECK_EQUAL(play(c9999, smarty, 4, 100) <= 100, true)
+		.CHECK_EQUAL(play(c2610, smarty, 4, 100) <= 100, true)
+		.CHECK_EQUAL(play(c0406, smarty, 4, 100) <= 100, true)
+
+	;
+
+
 
     grade = testcase.grade();
 	} else {
